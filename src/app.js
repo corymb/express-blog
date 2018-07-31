61c55f34e111929e9a16078bc3f2a19eef8abdfd
Dave Bush <github@david-bush.co.uk>
2018-07-31 19:07:29 +1200

    Added assets directory.

--- src/app.js
+++ src/app.js
@@ -3,8 +3,11 @@ const path = require('path');
 
 const app = express();
 
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'assets')));
 
 app.get('/', (req, res) => {
   res.render('index', { title: 'Blog', posts: [{ title: 'First Post!' }] });
