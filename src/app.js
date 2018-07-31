5c6e551594688fde6d6ace202f436c1bc83d75b0
Dave Bush <github@david-bush.co.uk>
2018-07-31 19:02:31 +1200

    Added templating engine.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  res.render('index', { title: 'Blog', posts: [{ title: 'First Post!' }] });
 });
 
 app.listen(3000);
