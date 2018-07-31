974051ea3f2a3f323bb24303bfc4f9aa6c66c4a8
Dave Bush <github@david-bush.co.uk>
2018-07-31 19:18:32 +1200

    Split out routing.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const path = require('path');
 
+const blogRouter = require('./routes/blog');
+
 const app = express();
 
 
+// View layer
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Static assets
 app.use(express.static(path.join(__dirname, 'assets')));
 
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Blog', posts: [{ title: 'First Post!' }] });
-});
-
+app.use('/', blogRouter);
 app.listen(3000);
